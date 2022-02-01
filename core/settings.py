e1a7fc3b5958b19c5af441c8abfdb43f3aaa2292
Bakdolot <genry03gh@gmail.com>
2022-02-02 00:10:28 +0600

    fix

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
