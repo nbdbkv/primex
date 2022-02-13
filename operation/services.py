ad1a4d1fb9c6e38ad74c385940f06ee34bee4b01
Bakdolot <genry03gh@gmail.com>
2022-02-13 12:49:07 +0600

    fixed bag

--- operation/services.py
+++ operation/services.py
@@ -7,10 +7,10 @@ from uuid import uuid4
 
 
 def get_parcel_code(direction: dict) -> str:
-    city = direction.get('city')
-    code = city.region.code + city.code
-    if district := direction.get('district'):
-        code += district.code
+    district = direction.get('district')
+    code = district.region.code + district.code
+    if village := direction.get('village'):
+        code += village.code
     code += str(uuid4())[:15-len(code)]
     return code
 
