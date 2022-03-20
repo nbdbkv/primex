8538247be2c52a99ec3821195bfa407372b93e44
Bakdolot <genry03gh@gmail.com>
2022-03-21 00:41:39 +0600

    updated envelop filter

--- operation/filters.py
+++ operation/filters.py
@@ -1,12 +1,24 @@
 from django_filters import FilterSet, NumberFilter
+from django.shortcuts import get_object_or_404
 
-from operation.models import Envelop
+from operation.models import Envelop, District
+
+
+def get_from_district (queryset, name, value):
+    lookup = 'distance__from_region'
+    region = get_object_or_404(District.objects, id=value).region
+    return queryset.filter(**{lookup: region})
 
 
 class EnvelopFilter(FilterSet):
-    from_district = NumberFilter(field_name='distance__from_region__district', lookup_expr='distance__from_region__district')
+    from_district = NumberFilter(method=get_from_district)
     to_district = NumberFilter(field_name='distance__to_district', lookup_expr='distance__to_district') 
 
     class Meta:
         model = Envelop 
         fields = ['from_district', 'to_district']
+
+    def get_from_district(self, queryset, name, value):
+        lookup = 'distance__from_region'
+        region = get_object_or_404(District.objects, id=value).region
+        return queryset.filter(**{lookup: region})
