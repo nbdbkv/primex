6d3f9430ddf6f7290d8eeb527fb3baa4be83fa35
abdulhatov <1704.01051@manas.edu.kg>
2022-06-15 16:27:01 +0600

    fix bag

--- operation/serializers.py
+++ operation/serializers.py
@@ -212,7 +212,7 @@ class ReatriveParcelSerializer(serializers.ModelSerializer):
 
 
 class BonusHistorySerializer(serializers.ModelSerializer):
-    sending_date = serializers.SerializerMethodField()
+    # sending_date = serializers.SerializerMethodField()
     code = serializers.SerializerMethodField()
     icon = serializers.SerializerMethodField()
     parcel_sum = serializers.SerializerMethodField()
@@ -233,8 +233,8 @@ class BonusHistorySerializer(serializers.ModelSerializer):
     def get_parcel_sum(self, instance):
         return instance.parcel.payment.price
 
-    def get_sending_date(self, instance):
-        return instance.parcel.sending_date
+    # def get_sending_date(self, instance):
+    #     return instance.parcel.sending_date
 
 
 class ImageSerializer(serializers.ModelSerializer):
