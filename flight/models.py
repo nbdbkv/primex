8de29cf17e3976d2a348458e71b6c4fbeca7d0c7
ArgenEsenov <argenesenov2002@gmail.com>
2023-03-20 17:50:50 +0600

    total

--- flight/models.py
+++ flight/models.py
@@ -24,10 +24,10 @@ class Flight(TimeStampedModel):
     numeration = models.CharField(db_index=True, max_length=64, verbose_name=_('Нумерация рейсов'))
     code = models.CharField(db_index=True, max_length=64, verbose_name=_('Код'))
     quantity = models.PositiveIntegerField(verbose_name=_('Количество мест (коробка)'))
-    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight'), null=True, blank=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight flight'), null=True, blank=True)
     cube = models.DecimalField(max_digits=9, decimal_places=2, verbose_name=_('cube'), null=True, blank=True)
     density = models.DecimalField(max_digits=9, decimal_places=2, verbose_name=_('density'), null=True, blank=True)
-    consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('consumption'), null=True,
+    consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('consumption flight'), null=True,
                                       blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('price'), null=True, blank=True)
     sum = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('sum'), null=True, blank=True)
@@ -45,8 +45,8 @@ class Box(TimeStampedModel):
                                null=True, blank=True)
     code = models.CharField(db_index=True, max_length=64, verbose_name=_('Код'))
     track_code = models.CharField(db_index=True, max_length=64, verbose_name=_('Трек-Код'))
-    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight'), null=True, blank=True)
-    consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('consumption'), null=True,
+    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight box'), null=True, blank=True)
+    consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('consumption box'), null=True,
                                       blank=True)
 
     class Meta:
@@ -60,12 +60,12 @@ class BaseParcel(TimeStampedModel):
                             null=True, blank=True)
     code = models.CharField(db_index=True, max_length=64, verbose_name=_('Код'))
     track_code = models.CharField(db_index=True, max_length=64, verbose_name=_('Трек-Код'))
-    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight'))
+    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('weight box'))
 
     # Габариты
-    width = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('width'))
-    length = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('length'))
-    height = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('height'))
+    width = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('width base'))
+    length = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('length base'))
+    height = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('height base'))
 
     comment = models.TextField(verbose_name=_('comment'), null=True, blank=True)
 
