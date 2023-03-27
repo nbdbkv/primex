ebbd9ed1afdada05f22a9fe35e5c97e493f60f3b
Timur Obolbekov <you@example.com>
2023-03-27 15:49:11 +0600

    import export box

--- flight/models.py
+++ flight/models.py
@@ -29,8 +29,6 @@ class Flight(TimeStampedModel):
     density = models.DecimalField(max_digits=9, decimal_places=2, verbose_name=_('Плотность'), null=True, blank=True)
     consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Расход'), null=True,
                                       blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Цена'), null=True, blank=True)
-    sum = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Сумма'), null=True, blank=True)
     status = models.PositiveIntegerField(default=LOW, choices=STATUS_CHOICES, verbose_name=_('Статус'),
                                          null=True, blank=True)
 
@@ -49,11 +47,14 @@ class Box(TimeStampedModel):
     # Коробка
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='box', verbose_name=_('Рейс коробки'),
                                null=True, blank=True)
-    code = models.CharField(db_index=True, max_length=64, verbose_name=_('Код'))
-    track_code = models.CharField(db_index=True, max_length=64, verbose_name=_('Трек-Код'))
-    weight = models.DecimalField(max_digits=10, decimal_places=3, verbose_name=_('Вес'), null=True, blank=True)
-    consumption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Расход'), null=True,
+    code = models.CharField(max_length=64, verbose_name=_('Код'), null=True, blank=True)
+    track_code = models.CharField(max_length=64, verbose_name=_('Трек-Код'), null=True, blank=True)
+    weight = models.CharField(max_length=64, verbose_name=_('Вес'), null=True, blank=True)
+    price = models.CharField(max_length=64, verbose_name=_('Цена'), null=True, blank=True)
+    consumption = models.CharField(max_length=64, verbose_name=_('Расход'), null=True,
                                       blank=True)
+    sum = models.CharField(max_length=64, verbose_name=_('Сумма'), null=True, blank=True)
+    comment = models.TextField(max_length=64, verbose_name=_('comment'), null=True, blank=True)
 
     class Meta:
         verbose_name = _('box')
@@ -68,7 +69,8 @@ class Box(TimeStampedModel):
 
 class BaseParcel(TimeStampedModel):
     # Посылка
-    box = models.ForeignKey(Box, on_delete=models.CASCADE, related_name='base_parcel', verbose_name=_('Коробка посылки'),
+    box = models.ForeignKey(Box, on_delete=models.CASCADE, related_name='base_parcel',
+                            verbose_name=_('Коробка посылки'),
                             null=True, blank=True)
     code = models.CharField(db_index=True, max_length=64, verbose_name=_('Код'))
     track_code = models.CharField(db_index=True, max_length=64, verbose_name=_('Трек-Код'))
@@ -78,8 +80,6 @@ class BaseParcel(TimeStampedModel):
     width = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Ширина'))
     length = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Длина'))
     height = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Высота'))
-
-    comment = models.TextField(verbose_name=_('comment'), null=True, blank=True)
 
     class Meta:
         verbose_name = _('base_parcel')
