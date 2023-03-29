f424af9ed0b13651c46976aeac5dd268191ed58f
ArgenEsenov <argenesenov2002@gmail.com>
2023-03-29 15:43:56 +0600

    total1

--- flight/admin.py
+++ flight/admin.py
@@ -19,8 +19,19 @@ class BaseParcelInline(admin.StackedInline):
 @admin.register(Box)
 class BoxAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'code', 'track_code', 'weight', 'consumption')
-    exclude = ('box', )
+    exclude = ('box', 'weight')
     inlines = [BaseParcelInline]
 
+    def save_model(self, request, obj, form, change):
+        a = 0
+        sum = 0.0
+        for key, value in form.data.items():
+            if key == f'base_parcel-{a}-weight':
+                if value:
+                    sum += float(value)
+                    a += 1
+                    super().save_model(request, obj, form, change)
+        obj.weight = sum
+        super(BoxAdmin, self).save_model(request, obj, form, change)
 
 
