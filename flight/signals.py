e7d80340743ebace557644a8e02b217e8dcb0647
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2023-06-21 19:35:37 +0600

    push notification on arriving of baseparcel

--- flight/signals.py
+++ flight/signals.py
@@ -9,7 +9,7 @@ from flight.models import BaseParcel
 
 
 @receiver(post_save, sender=BaseParcel)
-def send_fcm_on_create(sender, instance, created, **kwargs):
+def send_fcm_when_forming(sender, instance, created, **kwargs):
     if created:
         user = User.objects.exclude(code_logistic=None).filter(
             Q(code_logistic=instance.client_code) | (Q(phone=instance.phone))
@@ -20,7 +20,25 @@ def send_fcm_on_create(sender, instance, created, **kwargs):
                 device.send_message(
                     Message(
                         notification=Notification(
-                            title='Taura Express', body=f'Посылка {instance.track_code} формируется на складе в Китае'
+                            title='Taura Express', body=f'Посылка {instance.track_code} формируется на складе в Китае.'
+                        )
+                    )
+                )
+
+
+@receiver(post_save, sender=BaseParcel)
+def send_fcm_on_arrival(sender, instance, created, **kwargs):
+    if not created and instance.status == 2:
+        user = User.objects.exclude(code_logistic=None).filter(
+            Q(code_logistic=instance.client_code) | (Q(phone=instance.phone))
+        ).values('pk')
+        if user:
+            devices = FCMDevice.objects.filter(user_id__in=user)
+            for device in devices:
+                device.send_message(
+                    Message(
+                        notification=Notification(
+                            title='Taura Express', body=f'Посылка {instance.track_code} прибыла на склад в КР.'
                         )
                     )
                 )
