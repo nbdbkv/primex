71142e39976a5ca6106ede5e6df245dd8829b5c2
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2023-06-21 19:39:55 +0600

    push notification when baseparcel is ready

--- flight/signals.py
+++ flight/signals.py
@@ -42,3 +42,21 @@ def send_fcm_on_arrival(sender, instance, created, **kwargs):
                         )
                     )
                 )
+
+
+@receiver(post_save, sender=BaseParcel)
+def send_fcm_when_ready(sender, instance, created, **kwargs):
+    if not created and instance.status == 4:
+        user = User.objects.exclude(code_logistic=None).filter(
+            Q(code_logistic=instance.client_code) | (Q(phone=instance.phone))
+        ).values('pk')
+        if user:
+            devices = FCMDevice.objects.filter(user_id__in=user)
+            for device in devices:
+                device.send_message(
+                    Message(
+                        notification=Notification(
+                            title='Taura Express', body=f'Посылка {instance.track_code} раздается. Заберите посылку.'
+                        )
+                    )
+                )
