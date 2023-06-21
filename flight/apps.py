f5d0b7342829ff206f3195ede5c71f060f049ba2
Nurbakyt Abdibekov <nurbakyt.abdibekov@gmail.com>
2023-06-21 19:04:30 +0600

    push notification when creating baseparcel

--- flight/apps.py
+++ flight/apps.py
@@ -6,3 +6,6 @@ class FlightConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'flight'
     verbose_name = _('Flights')
+
+    def ready(self):
+        import flight.signals
---
+++ flight/signals.py
@@ -0,0 +1,26 @@
+from django.db.models import Q
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from fcm_django.models import FCMDevice
+from firebase_admin.messaging import Message, Notification
+
+from account.models import User
+from flight.models import BaseParcel
+
+
+@receiver(post_save, sender=BaseParcel)
+def send_fcm_on_create(sender, instance, created, **kwargs):
+    if created:
+        user = User.objects.exclude(code_logistic=None).filter(
+            Q(code_logistic=instance.client_code) | (Q(phone=instance.phone))
+        ).values('pk')
+        if user:
+            devices = FCMDevice.objects.filter(user_id__in=user)
+            for device in devices:
+                device.send_message(
+                    Message(
+                        notification=Notification(
+                            title='Taura Express', body=f'Посылка {instance.track_code} формируется на складе в Китае'
+                        )
+                    )
+                )
